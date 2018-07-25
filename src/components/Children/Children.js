46043c82bde33cde3defab516a1d4caa3aee6192
Amanda Mitchell <wreedamitchell@gmail.com>
2018-07-24 21:26:57 -0500

    wip

--- src/components/Children/Children.js
+++ src/components/Children/Children.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './Children.css';
 
 class ChildComp extends React.Component {
+  addClickEvent = () => {
+    this.props.removeChild(this.props.details.id);
+  }
   render () {
     const {details} = this.props;
     return (
       <div className="children">
         <h2 className="child-name">{details.name}</h2>
+        <button className="btn btn-default glyphicon glyphicon-trash" alt="delete" onClick={this.addClickEvent}></button>
       </div>
     );
   }
